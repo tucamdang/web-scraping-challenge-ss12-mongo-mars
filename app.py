8470296628b62e574d8d584c4687d1b8d0043425
TuCamDang <dtucam@gmail.com>
2022-05-14 13:34:19 +0930

    t1

---
+++ app.py
@@ -0,0 +1,28 @@
+import scrape_mars
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+# from splinter import Browser
+
+app = Flask(__name__)
+
+# Use flask_PyMongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    #mars_data['facts']
+    #print("Mars Facts")
+    print (mars_data['facts'])
+    mars.update({},mars_data, upsert=True)
+    return redirect('/', code=302)
+    
+if __name__ == "__main__":
+    app.run()
